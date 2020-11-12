86febef47a8ca6953c386d5554e8a4b96b75ff86
Tejvinder <tejvinder.singh.toor@gmail.com>
2020-11-12 06:37:34 -0500

    test generator made

---
+++ ghidrall/generate-all-tests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+from pathlib import Path
+import shutil
+
+
+def run_chain(path, name, opt):
+    cmd = "gclang++ -o " + name + ".o -O" + opt + " " + path
+    args = cmd.split()
+    subprocess.check_output(args)
+
+    cmd = "get-bc " + name + ".o"
+    args = cmd.split()
+    subprocess.check_output(args)
+
+    cmd = "llvm-dis-10 " + name + ".bc"
+    args = cmd.split()
+    subprocess.check_output(args)
+
+
+dir_path = Path('results')
+if dir_path.exists() and dir_path.is_dir():
+    shutil.rmtree(dir_path)
+os.mkdir('results')
+os.chdir('results')
+for file in os.listdir("../latest_tests/src"):
+    if file.endswith(".cpp") and file != "apis.cpp":
+        file_name = file.split(".")[0]
+        file_path = os.path.join("latest_tests/src", file)
+        run_chain(file_path, file_name, "0")
+        run_chain(file_path, file_name, "1")
+        run_chain(file_path, file_name, "2")
+
